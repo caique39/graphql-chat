f51c4d343b02749b2df467fdaccc85019a454c5a
Kelvin CS <kelvinh.job@gmail.com>
2019-11-23 14:32:57 -0300

    Production build

---
+++ app/build/precache-manifest.b5da039a0774da832358f532c3afac57.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54bfb77e627a3480a7990b10942074a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "981a71000826707fdf46",
+    "url": "/static/css/main.11676f53.chunk.css"
+  },
+  {
+    "revision": "9f8ac37a0723cea60875",
+    "url": "/static/js/2.b2396c68.chunk.js"
+  },
+  {
+    "revision": "981a71000826707fdf46",
+    "url": "/static/js/main.8ed220c4.chunk.js"
+  },
+  {
+    "revision": "ea4f97f630860d89da71",
+    "url": "/static/js/runtime-main.4873c03b.js"
+  }
+]);
